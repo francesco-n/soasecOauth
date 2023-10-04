55f7784e9338cd1f9d95b007524571907b953c85
fratornix <francesco.negriolli@protonmail.com>
2023-10-04 14:01:24 +0200

    client creato

--- authorizationserver/src/main/java/com/example/demo/AuthorizationserverApplication.java
+++ authorizationserver/src/main/java/com/example/demo/AuthorizationserverApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class AuthorizationserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationserverApplication.class, args);
+	}
+	
+	@Bean
+	InMemoryUserDetailsManager inMemoryUserDetailsManager () {
+		var one = User.withDefaultPasswordEncoder().roles("admin").username("amministratore").password("adminpassword").build();
+		var two = User.withDefaultPasswordEncoder().roles("user").username("utente").password("password").build();
+		return new InMemoryUserDetailsManager(one, two);
+		
 	}
 
 }
