2ab5ccd8978b96ff55aed47db0f8cf88eed9d10a
FraHackerino <20029793@studenti.uniupo.it>
2023-10-04 23:33:45 +0200

    :(

---
+++ userrepository/userrepository/src/test/java/SOASECOAUTH/userrepository/UserrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package SOASECOAUTH.userrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserrepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
