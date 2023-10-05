f225f72be6e3b016cf7c58f88aeef869ce3073da
fratornix <francesco.negriolli@protonmail.com>
2023-10-05 16:29:54 +0200

    database utenti

---
+++ authorizationserver/src/main/java/com/example/demo/AuthServerConfig.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class AuthServerConfig {
+
+    @Bean
+    EmbeddedDatabase datasource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("dashboard")
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    JdbcUserDetailsManager users(DataSource dataSource, PasswordEncoder encoder) {
+       UserDetails utente = User.builder()
+               .username("utente")
+               .password("{bcrypt}$2a$10$jdJGhzsiIqYFpjJiYWMl/eKDOd8vdyQis2aynmFN0dgJ53XvpzzwC")
+               .roles("USER")
+               .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(utente);
+        return jdbcUserDetailsManager;
+    }
+  
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- authorizationserver/src/main/java/com/example/demo/AuthorizationserverApplication.java
+++ authorizationserver/src/main/java/com/example/demo/AuthorizationserverApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AuthorizationserverApplication {
@@ -13,12 +15,12 @@ public class AuthorizationserverApplication {
 		SpringApplication.run(AuthorizationserverApplication.class, args);
 	}
 	
-	@Bean
-	InMemoryUserDetailsManager inMemoryUserDetailsManager () {
-		var one = User.withDefaultPasswordEncoder().roles("admin").username("amministratore").password("adminpassword").build();
-		var two = User.withDefaultPasswordEncoder().roles("user").username("utente").password("password").build();
-		return new InMemoryUserDetailsManager(one, two);
+	
+//	@Bean
+//	InMemoryUserDetailsManager inMemoryUserDetailsManager () {
+//		var one = User.withDefaultPasswordEncoder().roles("admin").username("amministratore").password("adminpassword").build();
+//		var two = User.withDefaultPasswordEncoder().roles("user").username("utente").password("password").build();
+//		return new InMemoryUserDetailsManager(one, two);
 		
-	}
-
+//	}
 }
